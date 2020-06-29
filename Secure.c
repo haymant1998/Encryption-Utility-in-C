5a952b72f362c650b1760d8a47df973e0bb40790
Haymant Mangla <haymant_1998@outlook.com>
2020-06-29 08:11:38 +0530

    Update Secure.c

--- Secure.c
+++ Secure.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "/Users/haymantmangla/Desktop/C/Project/headerSecure.c"
+#include "headerSecure.c"
 int main()
 {
     clrscr();
